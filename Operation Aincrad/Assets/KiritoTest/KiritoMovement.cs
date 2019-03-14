fc7e561088cdad84f676c0854ecbde9dbbaf2829
fairnightzz <zhehaizhang1@gmail.com>
2019-03-13 22:27:06 -0400

    ASUNA  AND KIRITO CAN MOVE PROPERLY

---
+++ Operation Aincrad/Assets/KiritoTest/KiritoMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KiritoMovement : MonoBehaviour
+{
+    private Animator kiritoController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        kiritoController = GetComponent<Animator>();
+        kiritoController.SetFloat("Vx", 0);
+        kiritoController.SetFloat("Vy", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
