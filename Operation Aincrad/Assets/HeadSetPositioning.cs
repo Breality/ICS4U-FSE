610d8e820932c6087dfdd403f0a60903beff42f0
Anish Aggarwal <anish.5@hotmail.com>
2019-03-14 21:14:53 -0400

    Movement with Controllers

---
+++ Operation Aincrad/Assets/HeadSetPositioning.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HeadSetPositioning : MonoBehaviour
+{
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputTracking.disablePositionalTracking = true;
+        this.transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
